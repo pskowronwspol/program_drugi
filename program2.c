edfa9b4305c01e84697f401faff20ff6dc8aa7c3
pskowronwspol <piotr.skowron101@onet.pl>
2017-10-19 17:48:13 +0200

    dwa kolejne programy

---
+++ program2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+int main()
+
+{
+
+	putchar('v');/* wypisz znak */
+
+	putchar('\n'); /* znak konca linii */
+
+        puts("wypisz string / ciag znakow");
+
+	puts("\nAla ma kota,\na kot ma Ale");
+
+	return 0;
+
+}
+
+
+
+
